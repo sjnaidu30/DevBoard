02aa2304bff443186d29822fb175c456954c6506
Jigisha <sjnaidu30@gmail.com>
2026-06-14 21:49:09 +0530

    feat: add express server entry point

--- server/index.js
+++ server/index.js
@@ -1,1 +1,18 @@
- 
+import express, { response } from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("DevBoaard API is running");
+});
+app.listen(PORT, () => {
+  console.log(`Server is running on ${PORT}`);
+});
