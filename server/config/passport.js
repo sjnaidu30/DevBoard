5fd2a4210de43e78a3fa8f4fe5654839bd39e247
Jigisha N <sjnaidu30@gmail.com>
2026-08-21 19:45:59 +0530

    fix: use env variable for OAuth callback URL

--- server/config/passport.js
+++ server/config/passport.js
@@ -10,7 +10,7 @@ passport.use(
     {
       clientID: process.env.GITHUB_CLIENT_ID,
       clientSecret: process.env.GITHUB_CLIENT_SECRET,
-      callbackURL: "http://localhost:5000/auth/github/callback",
+callbackURL: `${process.env.RAILWAY_URL}/auth/github/callback`,
     },
     async (accessToken, refreshToken, profile, done) => {
       try {
